df07403ec11c21526ca02048924c8b535c9db4bd
John Penaflor <johnvin.penaflor@gmail.com>
2025-03-30 18:11:48 -0600

    Created chat scroll hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -4,10 +4,11 @@ import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
-import { Fragment } from "react";
+import { Fragment, useRef, ElementRef } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
 import { useChatSocket } from "@/hooks/use-chat-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -44,6 +45,9 @@ export const ChatMessages = ({
     const addKey = `chat:${chatId}:messages`
     const updateKey = `chat:${chatId}:messages:update`
 
+    const chatRef = useRef<ElementRef<"div">>(null) as React.RefObject<HTMLDivElement>;
+    const bottomRef = useRef<ElementRef<"div">>(null) as React.RefObject<HTMLDivElement>;
+
     const {
         data,
         fetchNextPage,
@@ -57,7 +61,17 @@ export const ChatMessages = ({
         paramValue,
     });
 
-    useChatSocket({ queryKey, addKey, updateKey})
+    //activate chat socket hook here where user updates, deletes and gets message in real-time
+    useChatSocket({ queryKey, addKey, updateKey});
+
+    //activate chat scroll hook here
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0].items?.length ?? 0,
+    })
     //@ts-ignore
     if (status === "loading") {
         return (
@@ -83,12 +97,31 @@ export const ChatMessages = ({
 
 
     return (
-        <div className="flex-1 flex flex-col py-5 overflow-y-auto">
-            <div className="flex-1" />
-                <ChatWelcome 
-                    type={type}
-                    name={name}
-                />
+        <div ref={chatRef} className="flex-1 flex flex-col py-5 overflow-y-auto">
+            {!hasNextPage && <div className="flex-1" />}
+            {!hasNextPage && (<ChatWelcome 
+                type={type}
+                name={name}
+            />
+            )}
+            {hasNextPage && (
+                <div className="flex justify-center">
+                    {isFetchingNextPage ? (
+                        <Loader2
+                            className="h-6 w-6 text-zinc-500 animate-spin my-4" 
+                        />
+                    ) : (
+                        <button
+                            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 
+                            dark:hover:text-zinc-300 transition"
+                        >
+                            Load previous messages
+                        </button>
+                    )}
+
+                </div>
+
+            )}
             <div className="flex flex-col-reverse mt-auto">
                 {data?.pages?.map((group, i) => ( 
                     
@@ -117,6 +150,7 @@ export const ChatMessages = ({
             ))}
 
             </div>
+            <div ref={bottomRef}/>
         </div>
     )
 }
---
+++ hooks/use-chat-scroll.ts
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+type ChatScrollProps = {
+    chatRef: React.RefObject<HTMLDivElement>;
+    bottomRef: React.RefObject<HTMLDivElement>;
+    shouldLoadMore: boolean;
+    loadMore: () => void;
+    count: number;
+}
+
+export const useChatScroll = ({
+    chatRef,
+    bottomRef,
+    shouldLoadMore,
+    loadMore,
+    count,
+} : ChatScrollProps) => {
+    const [hasInitialized, setHasInitialized] = useState(false);
+
+    //useEffect for loading top messages
+    useEffect(() => {
+        const topDiv = chatRef?.current;
+
+        //event handler for loading top pages at the top of the scroll
+        const handleScroll = () => {
+            //get top div component and scroll to top
+            const scrollTop = topDiv?.scrollTop;
+            //if user is at the top of scroll, trigger load more function
+            if (scrollTop === 0 && shouldLoadMore) {
+                loadMore()
+            }
+        };
+
+        topDiv?.addEventListener("scroll", handleScroll);
+
+        return () => topDiv?.removeEventListener("scroll", handleScroll);
+
+    }, [shouldLoadMore, loadMore, chatRef]);
+
+    //useEffect for loading bottom messages
+    useEffect(() => {
+        const bottomDiv = bottomRef?.current;
+        const topDiv = chatRef.current;
+
+        const shouldAutoScroll = () => {
+            //if not initialized and user is at bottom div component, set has initialized to true and return true
+            if (!hasInitialized && bottomDiv) {
+                setHasInitialized(true);
+                return true;
+            }
+
+            //if top div doesn't exist
+            if (!topDiv) {
+                return false;
+            }
+
+            //calculate distance from bottom page
+            const distanceFromBottom = topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+            return distanceFromBottom <= 100;
+        }
+
+        if (shouldAutoScroll()) {
+            setTimeout(() => {
+                bottomRef.current?.scrollIntoView({
+                    behavior: "smooth",
+                });
+            }, 100)
+        }
+    }, [bottomRef, chatRef, count, hasInitialized]);
+
+
+}
